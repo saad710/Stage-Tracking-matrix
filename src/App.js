bf7fbbad8491e03bca7ab9017b814c86836b7a69
Md. Ifrat Hasnat <ifrathasnat2@gmail.com>
2022-02-20 13:23:59 +0600

    stage-track

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import moment from 'moment';
+import StageTable from './StageTable';
+
+const ZOHO = window.ZOHO;
 
 function App() {
+  const [zohoInitialized, setZohoInitialized] = useState(false);
+  const [entity, setEntity] = useState("");
+  const [entityId, setEntityId] = useState("");
+  const [stageHistory, setStageHistory] = useState([]);
+  useEffect(()=> {
+    /*
+    * Subscribe to the EmbeddedApp onPageLoad event before initializing 
+    */
+    
+    ZOHO.embeddedApp.on("PageLoad",function(data)
+    {
+      console.log(data);
+      setEntity(data.Entity);
+      setEntityId(data.EntityId)
+      //Custom Bussiness logic goes here
+    })
+
+    /*
+    * initializing the widget.
+    */
+    ZOHO.embeddedApp.init().then( () => {
+      setZohoInitialized(true);
+    });
+  }, [])
+
+  useEffect(async () => {
+    if(zohoInitialized && entity !== "" && entityId !== ""){
+      const stageHistoryResp = await ZOHO.CRM.API.getRelatedRecords({Entity:entity,RecordID:entityId,RelatedList:"Stage_History",page:1,per_page:200})
+      console.log("Related List Data", stageHistoryResp?.data)
+      setStageHistory(stageHistoryResp?.data);
+    }
+
+  }, [zohoInitialized, entity, entityId])
+
+  const lengthData = stageHistory?.length
+
+  
+  let putStage = []
+  let loopCount = 0
+  const stageTrack = (tempArray) => {
+    let stageArr = []
+    let firstStage = tempArray[loopCount];
+    let objData = {}
+    tempArray.forEach((temp,index) => {
+      //  console.log(temp.Last_Modified_Time)
+       const dateData = Math.abs((new Date(firstStage.Last_Modified_Time).getTime() - new Date(temp.Last_Modified_Time).getTime()) / 1000);
+     
+       console.log(dateData)
+       objData = {[temp.Stage] : dateData}
+       console.log(objData)
+       stageArr.push({...objData})
+       console.log(stageArr)
+       console.log(firstStage?.Stage)
+      //  firstStage = temp;
+    })
+    // console.log(stageArr)
+   const findData = {[firstStage?.Stage] : stageArr}
+   console.log(findData)
+   putStage.push(findData);
+  
+  console.log(putStage)
+  //  console.log(findData)
+         if(putStage.length < lengthData ){
+           loopCount = loopCount + 1;
+            stageTrack(tempArray);
+         }
+   return putStage;
+  }
+  
+  //final-result
+const result = stageTrack(stageHistory)
+console.log(result)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <StageTable result= {result}/>
     </div>
   );
 }
---
+++ src/StageTable.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StageTable = (props) => {
+    const {result} = props;
+    console.log(result)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default StageTable;
