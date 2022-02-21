b873df6dc646f37d699cfbff61116e86fab44f84
saad710 <ifrathasnat2@gmail.com>
2022-02-21 15:03:53 +0600

    integration-done

--- src/App.js
+++ src/App.js
@@ -1,90 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
-import moment from 'moment';
-import StageTable from './StageTable';
+import logo from './logo.svg'
+import './App.css'
+import { useEffect, useState } from 'react'
+import moment from 'moment'
+import StageTable from './StageTable'
 
-const ZOHO = window.ZOHO;
+const ZOHO = window.ZOHO
 
 function App() {
-  const [zohoInitialized, setZohoInitialized] = useState(false);
-  const [entity, setEntity] = useState("");
-  const [entityId, setEntityId] = useState("");
-  const [stageHistory, setStageHistory] = useState([]);
-  useEffect(()=> {
+  const [zohoInitialized, setZohoInitialized] = useState(false)
+  const [entity, setEntity] = useState('')
+  const [entityId, setEntityId] = useState('')
+  const [stageHistory, setStageHistory] = useState([])
+  useEffect(() => {
     /*
-    * Subscribe to the EmbeddedApp onPageLoad event before initializing 
-    */
-    
-    ZOHO.embeddedApp.on("PageLoad",function(data)
-    {
-      console.log(data);
-      setEntity(data.Entity);
+     * Subscribe to the EmbeddedApp onPageLoad event before initializing
+     */
+
+    ZOHO.embeddedApp.on('PageLoad', function (data) {
+      console.log(data)
+      setEntity(data.Entity)
       setEntityId(data.EntityId)
       //Custom Bussiness logic goes here
     })
 
     /*
-    * initializing the widget.
-    */
-    ZOHO.embeddedApp.init().then( () => {
-      setZohoInitialized(true);
-    });
+     * initializing the widget.
+     */
+    ZOHO.embeddedApp.init().then(() => {
+      setZohoInitialized(true)
+    })
   }, [])
 
   useEffect(async () => {
-    if(zohoInitialized && entity !== "" && entityId !== ""){
-      const stageHistoryResp = await ZOHO.CRM.API.getRelatedRecords({Entity:entity,RecordID:entityId,RelatedList:"Stage_History",page:1,per_page:200})
-      console.log("Related List Data", stageHistoryResp?.data)
-      setStageHistory(stageHistoryResp?.data);
+    if (zohoInitialized && entity !== '' && entityId !== '') {
+      const stageHistoryResp = await ZOHO.CRM.API.getRelatedRecords({
+        Entity: entity,
+        RecordID: entityId,
+        RelatedList: 'Stage_History',
+        page: 1,
+        per_page: 200,
+      })
+      console.log('Related List Data', stageHistoryResp?.data)
+      setStageHistory(stageHistoryResp?.data)
     }
-
   }, [zohoInitialized, entity, entityId])
 
   const lengthData = stageHistory?.length
 
-  
   let putStage = []
   let loopCount = 0
   const stageTrack = (tempArray) => {
     let stageArr = []
-    let firstStage = tempArray[loopCount];
+    let firstStage = tempArray[loopCount]
     let objData = {}
-    tempArray.forEach((temp,index) => {
+    tempArray.forEach((temp, index) => {
       //  console.log(temp.Last_Modified_Time)
-       const dateData = Math.abs((new Date(firstStage.Last_Modified_Time).getTime() - new Date(temp.Last_Modified_Time).getTime()) / 1000);
-     
-       console.log(dateData)
-       objData = {[temp.Stage] : dateData}
-       console.log(objData)
-       stageArr.push({...objData})
-       console.log(stageArr)
-       console.log(firstStage?.Stage)
+      const dateData = Math.abs(
+        (new Date(firstStage.Last_Modified_Time).getTime() -
+          new Date(temp.Last_Modified_Time).getTime()) /
+          1000,
+      )
+
+      console.log(dateData)
+      objData = { [temp.Stage]: dateData }
+      console.log(objData)
+      stageArr.push({ ...objData })
+      console.log(stageArr)
+      console.log(firstStage?.Stage)
       //  firstStage = temp;
     })
     // console.log(stageArr)
-   const findData = {[firstStage?.Stage] : stageArr}
-   console.log(findData)
-   putStage.push(findData);
-  
-  console.log(putStage)
-  //  console.log(findData)
-         if(putStage.length < lengthData ){
-           loopCount = loopCount + 1;
-            stageTrack(tempArray);
-         }
-   return putStage;
+    const findData = { [firstStage?.Stage]: stageArr }
+    console.log(findData)
+    putStage.push(findData)
+
+    console.log(putStage)
+    //  console.log(findData)
+    if (putStage.length < lengthData) {
+      loopCount = loopCount + 1
+      stageTrack(tempArray)
+    }
+    return putStage
   }
-  
+
   //final-result
-const result = stageTrack(stageHistory)
-console.log(result)
+  const result = stageTrack(stageHistory)
+  // console.log(result[0]["5188282000000006805"])
+
+  let headKey = []
+  result?.forEach((ob) => {
+    headKey.push(...Object.keys(ob))
+  })
+  console.log(headKey)
+
+  let tableValue = []
+  headKey?.forEach((val) => {
+    result?.forEach((data) => {
+      if (data[val] !== undefined) {
+        // console.log(Object.assign({}, ...data[val]))
+        tableValue.push(Object.assign({}, ...data[val]))
+      }
+    })
+  })
+  console.log(tableValue)
 
   return (
     <div className="App">
-        <StageTable result= {result}/>
+      <StageTable result={result} headKey={headKey} tableValue={tableValue} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/StageTable.jsx
+++ src/StageTable.jsx
@@ -1,13 +1,75 @@
-import React from 'react';
+import * as React from 'react'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
 
 const StageTable = (props) => {
-    const {result} = props;
-    console.log(result)
-    return (
-        <div>
-            
-        </div>
-    );
-};
+  const { result, headKey, tableValue } = props
+  console.log(headKey)
+  return (
+    <TableContainer component={Paper} sx={{ width: 1250, padding: 2 }}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead sx={{ backgroundColor: 'black' }}>
+          <TableRow>
+            <TableCell
+              sx={{
+                border: '3px solid #73c2fb',
+                color: 'white',
+                fontWeight: '600',
+              }}
+            >
+              stage
+            </TableCell>
+            {headKey?.map((headData) => (
+              <TableCell
+                sx={{
+                  border: '3px solid #73c2fb',
+                  color: 'white',
+                  fontWeight: '600',
+                }}
+              >
+                {headData}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {result?.map((row, dataindex) => (
+            <TableRow
+              key={dataindex}
+              // sx={{ '&:last-child td, &:last-child th': { border: 1 } }}
+            >
+              <TableCell
+                sx={{ border: '3px solid #73c2fb' }}
+                component="th"
+                scope="row"
+              >
+                {Object.keys(row)}
+              </TableCell>
+              {headKey.map((head) =>
+                tableValue?.map(
+                  (data, index) =>
+                    index === dataindex && (
+                      <TableCell
+                        sx={{ border: '3px solid  #73c2fb' }}
+                        key={index}
+                        align="left"
+                      >
+                        {data[head]}
+                      </TableCell>
+                    ),
+                ),
+              )}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
 
-export default StageTable;
+export default StageTable
